f03467beb3928a613064794d669a4c4b32609f7d
Nick MacCarthy <nmaccarthy@smb-web1.va.placester.net>
2016-04-12 18:39:43 +0000

    init commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# REDELK
+
+
+# What is it?
+In my environment we have a few different Redis clusters that we like to monitor.  While I found other tools out there like redis-stats, I needed someting that was easy to setup and easily scalable to monitor our Redis clusters.  By monitor I'm refering to output and calculations arond the ```INFO``` command in Redis.
+
+This script will will run forever and is ment to be controlled with something like supervisord.  Below is a sample supervisor config
+
+# Requirements
+* python 2.7
+* virtualenv
+* pip
+
+
+# Install
+1. git clone this repot
+2. ```cd <cloned_dir> && virtualenv env && source env/bin/activate ```
+3. ```pip install -r requirements.txt``` 
+4. Copy ```config.yml.example``` to ```config.yml``` and fill in your relevent info
+5. Run it with something like supervisor
+6. Install the optional kibana4 dashboard ( json included ) into your kibana 4 instance
+
+# Sample supervisord entry
+
+```
+[program:redelk]
+command=/opt/monitor/redis_mon/env/bin/python /opt/monitor/redis_mon/redelk.py
+directory=/opt/monitor/redis_mon
+autostart=true
+autorestart=true
+logfile=/var/log/supervisor/redelk.log
+```
---
+++ redelk.py
@@ -0,0 +1,87 @@
+import os
+import redis
+import time
+import pprint
+from collections import defaultdict
+from elasticsearch import Elasticsearch
+import arrow
+import Queue
+from multiprocessing.pool import ThreadPool
+import math
+import sys
+
+pp = pprint.PrettyPrinter(indent=4)
+
+SHOME = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+
+def load_conf():
+    import yaml
+    with open('{}/config.yml'.format(SHOME), 'r') as f:
+        doc = yaml.load(f)
+    return doc
+
+conf = load_conf()
+poll_interval = conf['script_args'].get('poll_interval', 1)
+
+es = Elasticsearch([conf['elasticsearch']['hosts']], **conf['elasticsearch']['args'])
+
+
+def indexit(d):
+    def get_index():
+        now = arrow.utcnow().format('YYYY.MM.DD')
+        return 'redis-stats-{}'.format(now)        
+    es = Elasticsearch(conf['elasticsearch']['hosts'], **conf['elasticsearch']['args'])   
+    return es.index(index=get_index(), doc_type='redis-stats', body=d)
+
+def ddiff(d1,d2):
+    d = {}
+
+    d['@timestamp'] = arrow.utcnow().datetime
+    d['hits_per_second'] = d2['keyspace_hits'] - d1['keyspace_hits']
+    d['misses_per_second'] = float(d2['keyspace_misses'] - d1['keyspace_misses'])
+    d['evictions_per_second'] = d2['evicted_keys'] - d1['evicted_keys']
+    d['expired_per_second'] = d2['expired_keys'] - d1['expired_keys']
+    d['commands_per_second'] = d2['total_commands_processed'] - d1['total_commands_processed']
+    d['connections_per_second'] = d2['total_connections_received'] - d1['total_connections_received']
+    d['key_count'] = d2['db0']['keys']
+    d['connected_clients'] = d2['connected_clients']
+    d['used_memory_rss'] = d2['used_memory_rss']
+    d['used_memory'] = d2['used_memory']
+    d['hit_percentage'] = d['hits_per_second'] / d['commands_per_second'] * 100
+    d['last_save_time_epoch'] = d2['rdb_last_save_time']
+    d['last_save_time'] = arrow.get(d['last_save_time_epoch']).datetime
+    d['cpu_usage'] = { 'user': math.ceil(d2['used_cpu_user'] - d1['used_cpu_user']), 'system': math.ceil(d2['used_cpu_sys'] - d1['used_cpu_sys']) }
+    d['cluster_info'] = [ 
+                        {'redis_version': d2['redis_version']},
+                        {'uptime_in_seconds': d2['uptime_in_seconds']},
+                        {'uptime_in_days': d2['uptime_in_days']}
+                    ]
+    return d
+
+
+def worker(host):
+    conn_args = conf['redis'].get(host)
+    conn_args['host'] = host
+    red = redis.Redis(**conn_args)
+    d1 = red.info()
+    time.sleep(1)
+    d2 = red.info()
+    diffed = ddiff(d1,d2)
+    diffed['host'] = host
+    #pp.pprint(diffed)
+    print indexit(diffed)
+
+def main():
+    conf = load_conf()
+    hosts = conf['redis']
+    print hosts
+    pool = ThreadPool(processes=5)
+    pool.map(worker, hosts)
+    pool.close()
+    pool.join()
+
+
+
+while True:
+    main()
+    time.sleep(poll_interval)
